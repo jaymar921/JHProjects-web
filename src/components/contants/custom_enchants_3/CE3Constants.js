468741202888d31915b6892dea8bd8153f023d94
JayMar921 <jayharronabejar@gmail.com>
2025-03-03 20:49:04 +0800

    Added Styling and Constants

---
+++ src/components/contants/custom_enchants_3/CE3Constants.js
@@ -0,0 +1,8 @@
+export const PluginInformation = {
+  title: "Custom Enchantments 3",
+  subtitle: "Elevate your server with Epic RPG Gameplay!",
+  author: "JayMar921",
+  authorSocial: "https://jayharronabejar.vercel.app/",
+  description:
+    "Take your server to the next level with Custom Enchantments 3! Unlock powerful enchantments, dive into an immersive RPG experience, and choose your path as a Warrior, Archer, or Mage, each with unique abilities and playstyles. But that's not all! Venture into custom-built structures, uncover exclusive loot, and enjoy fully customizable features tailored to your world. Bring adventure, strategy, and limitless customization to your players today!",
+};
--- src/components/pages/CE3Page.jsx
+++ src/components/pages/CE3Page.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { PluginInformation } from "../contants";
 
 function CE3Page() {
   useEffect(() => {
@@ -6,12 +7,24 @@ function CE3Page() {
   }, []);
 
   return (
-    <div className="flex place-items-center text-center justify-center h-screen">
-      <div className="w-fit">
-        <p>Ongoing Maintenance</p>
-        <a href="https://jaymar921.github.io/jaymar_plugin_wiki/custom_enchantments_3.html">
-          CE 3 Wiki
-        </a>
+    <div className="w-screen">
+      <div
+        className={`w-full h-[400px] flex justify-center place-items-center bannerImage`}
+      >
+        <div className="text-center border-2 rounded-xl p-8 bg-[rgba(0,0,0,0.8)] select-none">
+          <h1 className="text-[1.5em] md:text-[3.2em] font-bold text-lime-500">
+            {PluginInformation.title}
+          </h1>
+          <h3 className="text-xs md:text-xl text-yellow-700 font-bold select-none">
+            {PluginInformation.subtitle}
+          </h3>
+          <p className="pt-2 select-none font-bold text-xs md:text-xl">
+            By{" "}
+            <a href={PluginInformation.authorSocial} target="_blank">
+              {PluginInformation.author}
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
