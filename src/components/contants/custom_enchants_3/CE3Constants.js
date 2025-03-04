03def151be6d6d638696e2cb8789f897e9dc213e
JayMar921 <jayharronabejar@gmail.com>
2025-03-04 10:35:35 +0800

    Update webpage (Classes)

--- src/components/contants/custom_enchants_3/CE3Constants.js
+++ src/components/contants/custom_enchants_3/CE3Constants.js
@@ -9,4 +9,96 @@ export const PluginInformation = {
     "https://www.spigotmc.org/resources/%E2%9C%AFcustom-enchantments-2-1-16-1-18-%E2%9C%AF.89793/download?version=455311",
   buyLink: "https://www.spigotmc.org/resources/102275/",
   supportLink: "https://buymeacoffee.com/jaymar921",
+  classes: {
+    description:
+      "Shape your adventure with three powerful classes: Warrior, Archer, and Mage! Each path unlocks unique skills, buffs, and playstyles, bringing an epic RPG experience to your server. Dominate battles, master magic, or strike from the shadows, the choice is yours!",
+    command: "/ce skills",
+    class: [
+      {
+        title: "Warrior",
+        icon: "fa-solid fa-shield-halved",
+        color: "text-red-500",
+        description:
+          "Settle in the southern area of the Taiga Biomes. They have the best in attacking ability and endurance, but they're slow in speed and is inaccurate.",
+        subdescription: "(You'll be able to use Sword Enchantments)",
+        main_attribute: "Strength",
+        attributes: [
+          "Increase Physical Damage by 0.13",
+          "Increase Physical Defense by 0.25",
+          "Increase Health Regen by 0.015",
+        ],
+        skills: [
+          {
+            title: "SWORD MASTERY",
+            description: "Increase sword damage",
+          },
+          {
+            title: "BERSERK",
+            description: "Get bonus damage for 0.2 for every hp lost",
+          },
+          {
+            title: "PENETRATION",
+            description: "Ignore enemy Damage Resistance by 0.5%",
+          },
+        ],
+      },
+      {
+        title: "Archer",
+        icon: "fa-solid fa-shoe-prints",
+        color: "text-green-500",
+        description:
+          "Inhibit in the northern land of jungle biomes. They shun the use of heavy equipment as it restricts their mobility and agility. They are considered as the quickest and fastest of all the classes.",
+        subdescription: "(You'll be able to use Bow Enchantments)",
+        main_attribute: "Agility",
+        attributes: [
+          "Increase Movement Speed by 0.025",
+          "Increase Critical Chance by 2%",
+          "Increase Physical Damage by 0.125",
+        ],
+        skills: [
+          {
+            title: "BOW MASTERY",
+            description: "Increase bow damage",
+          },
+          {
+            title: "UNSWERVING SHOT",
+            description: "Increase bow range",
+          },
+          {
+            title: "SWIFT ESCAPE",
+            description: "Increases Chance to Evade/Ignore Damage",
+          },
+        ],
+      },
+      ,
+      {
+        title: "Mage",
+        icon: "fa-solid fa-hat-wizard",
+        color: "text-blue-500",
+        description:
+          "Occupies the lands of Snowy Plain. They manifest less physical ability than the other classes but has the strongest magical powers.",
+        subdescription: "(You'll be able to use Magic Enchantments)",
+        main_attribute: "Intelligence",
+        attributes: [
+          "Increase Mana Regeneration",
+          "Increase Magic Damage",
+          "Increase Magic Resistance",
+        ],
+        skills: [
+          {
+            title: "WIZARD MASTERY",
+            description: "Increase cassting range / spell radius by 0.2 blocks",
+          },
+          {
+            title: "HOURGLASS",
+            description: "Decrease magic enchantments cooldown",
+          },
+          {
+            title: "MANA CHARGE",
+            description: "Increases Mana Regeneration by 0.125",
+          },
+        ],
+      },
+    ],
+  },
 };
--- src/components/pages/CE3Page.jsx
+++ src/components/pages/CE3Page.jsx
@@ -1,11 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { PluginInformation } from "../contants";
 import { RedirectTo } from "../utils/PageUtility";
+import CE3_Classes from "./ce3_subcontent/CE3_Classes";
+import WindowWrap from "../modals/windowWrap";
 
 function CE3Page() {
+  const [subcontent, setSubcontent] = useState("none");
   useEffect(() => {
     document.title = "Custom Enchantments 3";
   }, []);
+
+  const closeWindow = () => {
+    setSubcontent("none");
+  };
+
+  const subContent = () => {
+    switch (subcontent) {
+      case "classes":
+        return <CE3_Classes />;
+      default:
+        return <></>;
+    }
+  };
+
+  const subContentWindow = () => {
+    if (subcontent !== "none")
+      return (
+        <WindowWrap close={closeWindow} title={subcontent}>
+          {subContent()}
+        </WindowWrap>
+      );
+    else <></>;
+  };
 
   return (
     <div className="w-full">
@@ -38,26 +64,102 @@ function CE3Page() {
         <div className="w-[80%] md:w-[50%] md:flex justify-evenly py-8">
           <div className="md:w-[200px] p-2 text-center border-2 border-slate-700 shadow-2xl rounded-md text-md">
             <p className="p-2 px-2">Download premium now!</p>
-            <button onClick={() => RedirectTo(PluginInformation.buyLink)}>
+            <button
+              className="rounded-xl"
+              onClick={() => RedirectTo(PluginInformation.buyLink)}
+            >
               <i class="fa-solid fa-cart-shopping"></i> Buy Plugin
             </button>
           </div>
           <div className="md:w-[200px] p-2 text-center border-2 border-slate-700 shadow-2xl rounded-md mt-4 md:mt-0">
             <p className="p-2 px-2">Unsure yet? You can try the plugin</p>
-            <button onClick={() => RedirectTo(PluginInformation.trialLink)}>
+            <button
+              className="rounded-xl"
+              onClick={() => RedirectTo(PluginInformation.trialLink)}
+            >
               <i class="fa-solid fa-file-arrow-down"></i> Try Plugin
             </button>
           </div>
           <div className="md:w-[200px] p-2 text-center border-2 border-slate-700 shadow-2xl rounded-md mt-4 md:mt-0">
             <p className="p-2 px-2">Show support by donating</p>
-            <button onClick={() => RedirectTo(PluginInformation.supportLink)}>
+            <button
+              className="rounded-xl"
+              onClick={() => RedirectTo(PluginInformation.supportLink)}
+            >
               <i class="fa-solid fa-shield-heart"></i> Support
             </button>
           </div>
         </div>
       </section>
 
-      <div className="text-center w-full py-20">
+      <section className="justify-items-center py-4">
+        <div className="w-[80%] md:w-[50%]">
+          <h3 className="text-[1.2em] md:text-[1.5em] text-purple-500 font-bold">
+            Plugin Features
+          </h3>
+        </div>
+
+        <div className="w-[80%] md:w-[50%] md:grid md:grid-cols-2 xl:grid-cols-3 md:gap-2 justify-evenly py-8 ">
+          <div className="col-span-1 p-2 text-center border-2 border-slate-700 shadow-2xl rounded-md text-md">
+            <p className="p-2 px-2">Player roles / class paths</p>
+            <button
+              className="rounded-xl"
+              onClick={() => setSubcontent("classes")}
+            >
+              <i class="fa-solid fa-hat-wizard"></i> Classes
+            </button>
+          </div>
+          <div className="col-span-1 p-2 text-center border-2 border-slate-700 shadow-2xl rounded-md mt-4 md:mt-0">
+            <p className="p-2 px-2">Setup Enchantment Shop</p>
+            <button
+              className="rounded-xl"
+              onClick={() => setSubcontent("shops")}
+            >
+              <i class="fa-solid fa-shop"></i> Shops
+            </button>
+          </div>
+          <div className="col-span-1  p-2 text-center border-2 border-slate-700 shadow-2xl rounded-md mt-4 md:mt-0">
+            <p className="p-2 px-2">Custom Enchantment List</p>
+            <button
+              className="rounded-xl"
+              onClick={() => setSubcontent("enchants")}
+            >
+              <i class="fa-solid fa-wand-magic-sparkles"></i> Enchants
+            </button>
+          </div>
+          <div className="col-span-1 p-2 text-center border-2 border-slate-700 shadow-2xl rounded-md mt-4 md:mt-0">
+            <p className="p-2 px-2">Custom Looting plots</p>
+            <button
+              className="rounded-xl"
+              onClick={() => setSubcontent("plots")}
+            >
+              <i class="fa-solid fa-city"></i> Plots
+            </button>
+          </div>
+          <div className="col-span-1 p-2 text-center border-2 border-slate-700 shadow-2xl rounded-md mt-4 md:mt-0">
+            <p className="p-2 px-2">Custom Items List</p>
+            <button
+              className="rounded-xl"
+              onClick={() => setSubcontent("items")}
+            >
+              <i class="fa-solid fa-cube"></i> Items
+            </button>
+          </div>
+          <div className="col-span-1 p-2 text-center border-2 border-slate-700 shadow-2xl rounded-md mt-4 md:mt-0">
+            <p className="p-2 px-2">Plugin Default Settings</p>
+            <button
+              className="rounded-xl"
+              onClick={() => setSubcontent("settings")}
+            >
+              <i class="fa-solid fa-gears"></i> Settings
+            </button>
+          </div>
+        </div>
+      </section>
+
+      <section className="w-full">{subContentWindow()}</section>
+
+      {/* <div className="text-center w-full py-20">
         <h3>Ongoing development</h3>
         <button
           onClick={() =>
@@ -68,7 +170,7 @@ function CE3Page() {
         >
           Plugin Wiki
         </button>
-      </div>
+      </div> */}
     </div>
   );
 }
