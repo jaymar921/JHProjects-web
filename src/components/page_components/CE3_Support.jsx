81dcced3f47c52e0dfcc7b211c59491445c2c5b1
JayMar921 <jayharronabejar@gmail.com>
2025-03-05 20:49:52 +0800

    Darken BG
    Co-authored-by: MikaPiaChu921 <abellanapia9@gmail.com>
    Signed-off-by: JayMar921 <jayharronabejar@gmail.com>

--- src/components/page_components/CE3_Support.jsx
+++ src/components/page_components/CE3_Support.jsx
@@ -8,7 +8,7 @@ function CE3_Support() {
       <div className="md:flex gap-2">
         {PluginInformation.supportLink.map((support) => {
           return (
-            <div className="border-2 border-gray-600 rounded-xl w-full bg-[rgba(0,0,0,0.2)] p-4 m-auto mt-4 shadow-xl">
+            <div className="border-2 border-gray-600 rounded-xl w-full bg-[rgba(0,0,0,0.3)] p-4 m-auto mt-4 shadow-xl">
               <h1 className="text-center">{support.title}</h1>
               <div className="text-center p-8">
                 <i className={`${support.icon} text-[4em] m-auto`}></i>
