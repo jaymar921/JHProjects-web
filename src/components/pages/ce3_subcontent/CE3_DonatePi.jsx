a7500eb6332b2862bfa14ccabe9345618cb10f0b
JayMar921 <jayharronabejar@gmail.com>
2025-04-25 14:00:59 +0800

    DonatePI.jsx img re-adjustment

--- src/components/pages/ce3_subcontent/CE3_DonatePi.jsx
+++ src/components/pages/ce3_subcontent/CE3_DonatePi.jsx
@@ -4,12 +4,12 @@ import PiDonate from "../../../assets/custom_enchants_3/pi_donate.jpeg";
 function CE3_DonatePi({ setSubcontent }) {
   return (
     <div className="items-center w-[100%]">
-      <div className="relative m-auto w-[90%] my-2">
+      <div className="relative m-auto w-[90%] md:w-[500px] my-2">
         <p className="text-purple-500">Wallet Address: </p>
         <textarea disabled className="w-[100%] resize-none px-4 text-gray-300">
           {import.meta.env.VITE_PI_WALLET_ADDRESS ?? ""}
         </textarea>
-        <img src={PiDonate} />
+        <img className="m-auto" src={PiDonate} />
       </div>
       <div className="text-center py-4">
         <button onClick={() => setSubcontent("support")}>BACK</button>
