158acdca112038e3c0713cceb36ce522321eadd5
JayMar921 <jayharronabejar@gmail.com>
2025-09-22 21:12:07 +0800

    Updated Trial Version

--- src/components/contants/custom_enchants_3/CE3Constants.js
+++ src/components/contants/custom_enchants_3/CE3Constants.js
@@ -16,8 +16,8 @@ export const PluginInformation = {
   description:
     "Take your server to the next level with Custom Enchantments 3! Unlock powerful enchantments, dive into an immersive RPG experience, and choose your path as a Warrior, Archer, or Mage, each with unique abilities and playstyles. But that's not all! Venture into custom-built structures, uncover exclusive loot, and enjoy fully customizable features tailored to your world. Bring adventure, strategy, and limitless customization to your players today!",
   trialLink:
-    "https://www.spigotmc.org/resources/%E2%9C%AFcustom-enchantments-3-1-16-free-%E2%9C%AF.89793/download?version=593302",
-  trialTitle: "v1.2.0-lite-r.2",
+    "https://www.spigotmc.org/resources/%E2%9C%AF-custom-enchantments-3-rpg-%E2%9C%AF-1-16-1-21-5-all-in-one-pugin.102275/",
+  trialTitle: "v1.2.0-lite-r.4",
   buyLink: "https://www.spigotmc.org/resources/102275/",
   paypalPaymentLink: "https://www.paypal.com/paypalme/JayMar921",
   supportLink: [
